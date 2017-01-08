c64045b5b5726453ada90491f12f338661ac0ec9
mclosos <mclosos@gmail.com>
2017-01-08 21:08:33 +0300

    added_case_with_same_currency

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 import test_cases
-
+#test_case_1
 test_cases.test_variable_sum("1,50")
 test_cases.test_variable_sum("100,00")
 test_cases.test_variable_sum("99999999999,00")
+
+#test_case_2
+test_cases.test_convert_rur_to_rur()
--- test_cases.py
+++ test_cases.py
@@ -5,12 +5,10 @@ import time
 from decimal import *
 
 def test_variable_sum(sum_to_convert):
-    #Test case 1
     #get firefox browser and open url
     browser =  webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
     browser.get("http://www.sberbank.ru/ru/quotes/converter")
     assert "Сбербанк" in browser.title
-    #sum_to_convert = "1.5"
     #wait a little.
     time.sleep(1.5)
 
@@ -37,3 +35,20 @@ def test_variable_sum(sum_to_convert):
     browser.quit()
 
 
+def test_convert_rur_to_rur():
+
+    browser = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+    browser.get("http://www.sberbank.ru/ru/quotes/converter")
+    assert "Сбербанк" in browser.title
+
+    time.sleep(1.5)
+    browser.find_element_by_xpath("//div[3]/div[2]/div/header/em").click()
+    time.sleep(1.5)
+    browser.find_element_by_xpath("//div[1]/div[1]/div[3]/div[2]/div/div/span[1]").click()
+    time.sleep(1.5)
+    browser.find_element_by_xpath("//div[4]/div[2]/div/header/em").click()
+    time.sleep(1.5)
+    browser.find_element_by_xpath("//div[4]/div[2]/div/div/span[1]").click()
+    time.sleep(1.5)
+    browser.find_element_by_xpath("//div[7]/button").click()
+    time.sleep(1.5)
