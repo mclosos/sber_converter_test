083cb90731089ae2cedb18019255528c32316e05
mclosos <mclosos@gmail.com>
2017-01-08 19:57:54 +0300

    added_init_and_brushed_test

---
+++ __init__.py
@@ -0,0 +1,5 @@
+import test_cases
+
+test_cases.test_variable_sum("1,50")
+test_cases.test_variable_sum("100,00")
+test_cases.test_variable_sum("99999999999,00")
--- test_cases.py
+++ test_cases.py
@@ -2,31 +2,38 @@
 
 from selenium import webdriver
 import time
+from decimal import *
 
-#get firefox browser and open url
-browser =  webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
-browser.get("http://www.sberbank.ru/ru/quotes/converter")
-assert "Сбербанк" in browser.title
-sum_to_convert = "1.5"
-#wait a little.
-time.sleep(1.5)
+def test_variable_sum(sum_to_convert):
+    #Test case 1
+    #get firefox browser and open url
+    browser =  webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+    browser.get("http://www.sberbank.ru/ru/quotes/converter")
+    assert "Сбербанк" in browser.title
+    #sum_to_convert = "1.5"
+    #wait a little.
+    time.sleep(1.5)
 
-sum_of_money = browser.find_element_by_xpath("//div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/aside/div[1]/div[1]/div[2]/div/form/input")
-sum_of_money.clear()
-sum_of_money.send_keys(sum_to_convert)
-time.sleep(1.5)
+    sum_of_money = browser.find_element_by_xpath("//div/form/input")
+    sum_of_money.clear()
+    sum_of_money.send_keys(sum_to_convert)
+    time.sleep(1.5)
+
+    browser.find_element_by_xpath("//div[7]/button").click()
+    time.sleep(1.5)
+
+    result = browser.find_element_by_xpath("//h4/span[1]").text
+    price = browser.find_element_by_xpath("//td[4]/span").text
+
+    sum_to_convert = sum_to_convert.replace(',', '.').replace(' ', '')
+    price = price.replace(',', '.').replace(' ', '')
+    result = result.replace(',', '.').replace(' ', '')
+
+    print(round(Decimal(sum_to_convert)/Decimal(price), 2), Decimal(result))
+    try:
+        assert (round(Decimal(sum_to_convert) / Decimal(price), 2) == Decimal(result))
+    except AssertionError:
+        print("Calculation mistake ", "expected:", round(Decimal(sum_to_convert)/Decimal(price), 2), ",actual:", result)
+    browser.quit()
 
 
-browser.find_element_by_xpath("//div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/aside/div[1]/div[7]/button").click()
-time.sleep(1.5)
-
-new_sum = browser.find_element_by_xpath("//div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/aside/div[1]/div[1]/div[2]/div/form/input").text
-result = browser.find_element_by_xpath("//div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/aside/div[2]/h4/span[1]").text
-price = browser.find_element_by_xpath(".//*[@id='main']/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div[1]/div/div[1]/table/tbody/tr/td[4]/span").text
-print(result)
-print(price)
-print(sum_to_convert)
-print(round(float(sum_to_convert.replace(',','.'))/float(price.replace(',','.')), 2) == float(result.replace(',','.')))
-assert (round(float(sum_to_convert.replace(',','.'))/float(price.replace(',','.')), 2) == float(result.replace(',','.')))
-
-browser.quit()
